256c31a4fce7e53b45d09e55da863386ee4b9934
lvzhenbang <lvzhenbang@outlook.com>
2019-01-24 11:54:41 +0800

    fix: it

--- pages/utils/service-workers.js
+++ pages/utils/service-workers.js
@@ -2,7 +2,7 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator
       .serviceWorker
-      .register('/service-worker.js')
+      .register('https://lvzhenbang.github.io/webpack4.x-multi-page/dist/service-worker.js')
       .then(registration => {
         console.log(`Yay! Workbox is loaded 🎉. ${registration}`);
       }).catch(registrationError => {
